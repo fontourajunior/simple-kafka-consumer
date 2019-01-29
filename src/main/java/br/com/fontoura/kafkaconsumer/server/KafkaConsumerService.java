1b684a551b69d0ca1bcd40d254c51b33a2cda007
Fontoura <antonio.fontoura@zup.com.br>
2019-01-29 18:08:09 -0200

    initial commit

---
+++ src/main/java/br/com/fontoura/kafkaconsumer/server/KafkaConsumerService.java
@@ -0,0 +1,12 @@
+package br.com.fontoura.kafkaconsumer.server;
+
+import org.springframework.kafka.annotation.KafkaListener;
+
+public class KafkaConsumerService {
+
+    @KafkaListener(topics = "t_test_one", groupId = "group_one")
+    public void listener(String message) {
+        System.out.println("consumer -> " + message);
+    }
+
+}
